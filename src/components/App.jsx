c52eef106ec40861fbc2b6f9a1d2f4d4adf0c10f
Svitlana-Ivanchuk <aig.sveta@gmail.com>
2023-08-20 01:31:13 +0300

    change metods

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,27 @@ export class App extends Component {
   state = {
     query: '',
     images: [],
+    page: 1,
   };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevState.query !== this.state.query ||
+      prevState.page !== this.state.page
+    ) {
+    }
+  }
+
   handleFormSubmit = newQuery => {
     console.log(newQuery);
     this.setState({
       query: newQuery,
+      images: [],
+      page: 1,
     });
+  };
+  handleLoadMore = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
   };
 
   render() {
@@ -23,7 +38,9 @@ export class App extends Component {
           <ImageGalleryItem></ImageGalleryItem>
         </ImageGallery>
         <div>
-          <button type="button">Load More</button>
+          <button type="button" onClick={this.handleLoadMore}>
+            Load More
+          </button>
         </div>
       </div>
     );
